54f19e99199ece1fbb719a5e6cc118c7cb40b047
favoured mwange <mwangefavoured@gmail.com>
2024-09-11 23:03:15 +0300

    Practices on inheritance lab1

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name='Favoured', last_name='Mwange'):
+        super().__init__(first_name,last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self,new_information):
+        # pass
+        if isinstance(new_information,str) and new_information:
+            self.knowledge.append(new_information)
